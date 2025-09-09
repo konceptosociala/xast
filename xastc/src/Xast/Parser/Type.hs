a413c574ec1d72e693f0e7e9e8795909256aaebc
Oleksandr Hnutov <sanerhp.mail@gmail.com>
2025-09-09 15:07:50 +0300

    Added: parse type definition

--- xastc/src/Xast/Parser/Type.hs
+++ xastc/src/Xast/Parser/Type.hs
@@ -1,47 +1,85 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards #-}
 
 module Xast.Parser.Type
-   ( TypeDef
-   , Type(..)
-   -- , typeDef
-   , parseType
+   ( TypeDef(..), typedef
+   , Type(..), type'
+   , Field(..), field
+   , Payload(..), payload
+   , Ctor(..), ctor
    ) where
 
 import Xast.Parser.Ident
-import Xast.Parser (Parser, lexeme, symbol)
-import Text.Megaparsec (choice, sepBy, between, some)
+import Xast.Parser (Parser, symbol, lexeme)
+import Text.Megaparsec (choice, sepBy, between, some, MonadParsec (try), many, sepBy1)
+import Data.Function ((&))
 
 data TypeDef = TypeDef
    { tdName       :: Ident
    , tdGenerics   :: [Ident]
    , tdCtors      :: [Ctor]
    }
+   deriving (Eq, Show)
+
+typedef :: Parser TypeDef
+typedef = do
+   _           <- symbol "type"
+   tdName      <- typeIdent
+   tdGenerics  <- many genericIdent
+   _           <- symbol "="
+   tdCtors     <- ctor `sepBy1` symbol "|"
+
+   return TypeDef {..}
 
 data Ctor = Ctor
    { ctorName     :: Ident
    , ctorPayload  :: Payload
    }
+   deriving (Eq, Show)
+
+ctor :: Parser Ctor
+ctor = do
+   ctorName    <- typeIdent
+   ctorPayload <- payload
+   return Ctor {..}
 
 data Payload
    = PUnit
    | PTuple [Type]
    | PRecord [Field]
-
-data Field = Field
-   { fldName :: Ident
-   , fldType :: Type
-   }
-
-data Type
-   = TyGnr Ident -- a, b, c...
-   | TyCon Ident -- Bool, Int, String
-   | TyApp Type Type -- Maybe a, Either a Int...
-   | TyTuple [Type] -- (Bool, a, Maybe String)
    deriving (Eq, Show)
 
-parseType :: Parser Type
-parseType = do
-   atoms <- some (lexeme atomType)
+payload :: Parser Payload
+payload = choice
+   [ PRecord   <$> between (symbol "{") (symbol "}") (field `sepBy` symbol ",")
+   , PTuple    <$> try (some (lexeme atomType))
+   , PUnit     & pure
+   ]
+
+data Field = Field      -- fieldOne : Int
+   { fldName :: Ident   -- field2 : Maybe Bool
+   , fldType :: Type
+   }
+   deriving (Eq, Show)
+
+field :: Parser Field
+field = do
+   fldName <- varIdent
+   _       <- symbol ":"
+   fldType <- type'
+
+   return Field {..}
+
+data Type
+   = TyGnr Ident     -- a, b, c...
+   | TyCon Ident     -- Bool, Int, String
+   | TyApp Type Type -- Maybe a, Either a Int...
+   | TyTuple [Type]  -- (Bool, a, Maybe String)
+   deriving (Eq, Show)
+
+type' :: Parser Type
+type' = do
+   atoms <- some atomType
    pure (foldl1 TyApp atoms)
 
 atomType :: Parser Type
@@ -53,7 +91,7 @@ atomType = choice
 
 tupleOrParens :: Parser Type
 tupleOrParens = between (symbol "(") (symbol ")") $ do
-   ts <- parseType `sepBy` symbol ","
+   ts <- type' `sepBy` symbol ","
    case ts of
       [] -> pure (TyTuple [])
       [t] -> pure t
--- xastc/test/Main.hs
+++ xastc/test/Main.hs
@@ -45,20 +45,151 @@ testVarIdent = TestCase $ do
 
 -- Typedef
 
-testParseType :: Test
-testParseType = TestCase $ do
-   assertParses parseType "a" (TyGnr (Ident "a"))
-   assertParses parseType "Bool" (TyCon (Ident "Bool"))
-   assertParses parseType "Maybe a" (TyApp (TyCon (Ident "Maybe")) (TyGnr (Ident "a")))
-   assertParses parseType "(Bool, a, Maybe String)" 
+testType :: Test
+testType = TestCase $ do
+   assertParses type' "a" (TyGnr (Ident "a"))
+   assertParses type' "Bool" (TyCon (Ident "Bool"))
+   assertParses type' "Maybe a" (TyApp (TyCon (Ident "Maybe")) (TyGnr (Ident "a")))
+   assertParses type' "(Bool, a, Maybe String)" 
       (TyTuple 
          [ TyCon (Ident "Bool")
          , TyGnr (Ident "a")
          , TyApp (TyCon (Ident "Maybe")) (TyCon (Ident "String"))
          ]
       )
-   assertFails parseType ""
-   assertFails parseType "(Bool, a, Maybe String"
+   assertFails type' ""
+   assertFails type' "(Bool, a, Maybe String"
+
+testParseField :: Test
+testParseField = TestCase $ do
+   assertParses field "fieldOne : Int"
+      (Field 
+         (Ident "fieldOne") 
+         (TyCon (Ident "Int"))
+      )
+   assertParses field "field2 : Maybe Bool"
+      (Field 
+         (Ident "field2") 
+         (TyApp (TyCon (Ident "Maybe")) (TyCon (Ident "Bool")))
+      )
+   assertFails field "fieldThree Int"
+   assertFails field "fieldFour :"
+
+testParsePayload :: Test
+testParsePayload = TestCase $ do
+   assertParses payload "" PUnit
+   assertParses payload "Int (Maybe Int) Bool" 
+      (PTuple 
+         [ TyCon (Ident "Int")
+         , TyApp (TyCon (Ident "Maybe")) (TyCon (Ident "Int"))
+         , TyCon (Ident "Bool")
+         ]
+      )
+   assertParses payload "{ fieldOne : Int, field2 : Either a b }"
+      (PRecord 
+         [ Field 
+            (Ident "fieldOne") 
+            (TyCon (Ident "Int"))
+         , Field 
+            (Ident "field2") 
+            (TyApp 
+               (TyApp (TyCon (Ident "Either")) (TyGnr (Ident "a"))) 
+               (TyGnr (Ident "b"))
+            )
+         ]
+      )
+
+   assertFails payload "{ fieldOne Int, field2 : Either a b }"
+   assertFails payload "{ fieldOne : Int, field2 : Either a b "
+
+testParseCtor :: Test
+testParseCtor = TestCase $ do
+   assertParses ctor "Just a"
+      (Ctor 
+         (Ident "Just")
+         (PTuple [TyGnr (Ident "a")])
+      )
+   assertParses ctor "Nothing"
+      (Ctor 
+         (Ident "Nothing")
+         PUnit
+      )
+   assertParses ctor "Node { left : Maybe (Tree a), right : Maybe (Tree a) }"
+      (Ctor 
+         (Ident "Node")
+         (PRecord 
+            [ Field 
+               (Ident "left")
+               (TyApp 
+                  (TyCon (Ident "Maybe"))
+                  (TyApp 
+                     (TyCon (Ident "Tree"))
+                     (TyGnr (Ident "a"))
+                  )
+               )
+            , Field 
+               (Ident "right")
+               (TyApp 
+                  (TyCon (Ident "Maybe"))
+                  (TyApp 
+                     (TyCon (Ident "Tree"))
+                     (TyGnr (Ident "a"))
+                  )
+               )
+            ]
+         )
+      )
+
+testParseTypeDef :: Test
+testParseTypeDef = TestCase $ do
+   assertParses typedef "type Maybe a = Just a | Nothing"
+      (TypeDef 
+         (Ident "Maybe")
+         [Ident "a"]
+         [ Ctor 
+            (Ident "Just")
+            (PTuple [TyGnr (Ident "a")])
+         , Ctor 
+            (Ident "Nothing")
+            PUnit
+         ]
+      )
+   assertParses typedef "type Tree a = Node { left : Maybe (Tree a), right : Maybe (Tree a) } | Leaf"
+      (TypeDef 
+         (Ident "Tree")
+         [Ident "a"]
+         [ Ctor 
+            (Ident "Node")
+            (PRecord 
+               [ Field 
+                  (Ident "left")
+                  (TyApp 
+                     (TyCon (Ident "Maybe"))
+                     (TyApp 
+                        (TyCon (Ident "Tree"))
+                        (TyGnr (Ident "a"))
+                     )
+                  )
+               , Field 
+                  (Ident "right")
+                  (TyApp 
+                     (TyCon (Ident "Maybe"))
+                     (TyApp 
+                        (TyCon (Ident "Tree"))
+                        (TyGnr (Ident "a"))
+                     )
+                  )
+               ]
+            )
+         , Ctor 
+            (Ident "Leaf")
+            PUnit
+         ]
+      )
+
+   assertFails typedef "type Maybe a Just a | Nothing"
+   assertFails typedef "type Maybe a = Just a | "
+   assertFails typedef "type Maybe a = | Nothing"
 
 -- Tests
 
@@ -68,7 +199,11 @@ tests = TestList
    , TestLabel "Parse type ident" testTypeIdent
    , TestLabel "Parse fn ident" testFnIdent
    , TestLabel "Parse var ident" testVarIdent
-   , TestLabel "Parse type" testParseType
+   , TestLabel "Parse type" testType
+   , TestLabel "Parse field" testParseField
+   , TestLabel "Parse payload" testParsePayload
+   , TestLabel "Parse ctor" testParseCtor
+   , TestLabel "Parse typedef" testParseTypeDef
    ]
 
 main :: IO ()
